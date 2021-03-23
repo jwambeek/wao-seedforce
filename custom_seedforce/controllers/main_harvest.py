2d33eb177b664cf871a91679477c949f5427f188
Ranjeet Kumar <ranjeet.kumar@vemo.com>
2021-03-23 13:02:25 +0530

    Update main_harvest.py

--- custom_seedforce/controllers/main_harvest.py
+++ custom_seedforce/controllers/main_harvest.py
@@ -6,7 +6,7 @@ class WebsiteSale(http.Controller):
     @http.route('/harvest_webform', type='http', auth="public", website=True)
     def harvest_webform(self, **post):
         print("Thanks for submission")
-        return http.render('custom_seedforce.sale_order_view', {})
+        #return http.render('custom_seedforce.sale_order_view', {})
 
 
     @http.route('/harvest_webform/submit', type='http', auth="public", website=True)
