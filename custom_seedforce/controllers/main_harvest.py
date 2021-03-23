2a1655ce2fbb129aa37e4946c67e87cc193f35a2
Ranjeet Kumar <ranjeet.kumar@vemo.com>
2021-03-23 13:36:22 +0530

    Update main_harvest.py

--- custom_seedforce/controllers/main_harvest.py
+++ custom_seedforce/controllers/main_harvest.py
@@ -4,22 +4,22 @@ from odoo.http import request
 class WebsiteSale(http.Controller):
 
     @http.route('/harvest_webform', type='http', auth="public", website=True)
-    def harvest_webform(self, **post):
+    def harvest_webform(self, **kw):
         #return "Thanks for submission"
         return http.render('custom_seedforce.sale_order_view', {})
 
 
-    @http.route('/harvest_webform/submit', type='http', auth="public", website=True)
-    def sale_order(self, **post):
-        #request.env['sale.order'].sudo().create(kw)
-        #return request.render("custom_seedforce.harvest_success", {})
+    # @http.route('/harvest_webform/submit', type='http', auth="public", website=True)
+    # def sale_order(self, **post):
+    #     #request.env['sale.order'].sudo().create(kw)
+    #     #return request.render("custom_seedforce.harvest_success", {})
 
-        partner = request.env['sale.order'].create({
-            'partner_id': post.get('partner_id')
+    #     partner = request.env['sale.order'].create({
+    #         'partner_id': post.get('partner_id')
 
-        })
-        vals = {
-            'partner': partner,
-        }
+    #     })
+    #     vals = {
+    #         'partner': partner,
+    #     }
 
         #return request.render("custom_seedforce.harvest_success", vals)
