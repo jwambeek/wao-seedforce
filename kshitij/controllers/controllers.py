31a154f2076b7bd9d777c9ec371e10673f1ebc4a
Kshitij Saini <kshitij.saini.astrea@gmail.com>
2021-03-24 16:06:09 +0530

    Update controllers.py

--- kshitij/controllers/controllers.py
+++ kshitij/controllers/controllers.py
@@ -6,6 +6,6 @@ from odoo.addons.website_form.controllers.main import WebsiteForm
 
 
 class Hospital(WebsiteForm):
-    @http.route('/website_form/', type='http', auth="public", methods=['POST'], multilang=False)
+    @http.route('/website_form', type='http', auth="public", methods=['POST'], multilang=False)
     def website_form_empty(self, **kwargs):
         return ""
