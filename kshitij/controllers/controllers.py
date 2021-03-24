8311b3c8b1469c7f9767a426afa9032e042b0ed4
Kshitij Saini <kshitij.saini.astrea@gmail.com>
2021-03-24 15:16:38 +0530

    testing controller

--- kshitij/controllers/controllers.py
+++ kshitij/controllers/controllers.py
@@ -1,21 +1,10 @@
 # -*- coding: utf-8 -*-
 # from odoo import http
+from odoo import http
+from odoo.http import request
 
+class Hospital(http.Controller):
+    @http.route('/website/res.partner',type="http",auth="public",website=True)
+    def patient_web(self,**kwargs):
+        print(kwargs)
 
-# class Kshitij(http.Controller):
-#     @http.route('/kshitij/kshitij/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
-
-#     @http.route('/kshitij/kshitij/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('kshitij.listing', {
-#             'root': '/kshitij/kshitij',
-#             'objects': http.request.env['kshitij.kshitij'].search([]),
-#         })
-
-#     @http.route('/kshitij/kshitij/objects/<model("kshitij.kshitij"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('kshitij.object', {
-#             'object': obj
-#         })
