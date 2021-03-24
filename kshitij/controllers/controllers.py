621a5f9979680398242cba61fcc9bb3b04dada77
Kshitij Saini <kshitij.saini.astrea@gmail.com>
2021-03-24 16:25:52 +0530

    Update controllers.py

--- kshitij/controllers/controllers.py
+++ kshitij/controllers/controllers.py
@@ -4,14 +4,16 @@ from odoo import http
 from odoo.http import request
 from odoo.addons.website_form.controllers.main import WebsiteForm
 
+def ksh(t):
+    print("Khs")
 
 class SubmissionTest(WebsiteForm):
     @http.route('/website_form', type='http', auth="public", methods=['POST'], multilang=False)
     def website_form_empty(self, **kwargs):
         print(kwargs)
-        return 200
+        return ksh("noting")
 
     @http.route('/website_form/<string:model_name>', type='http', auth="public", methods=['POST'], website=True, csrf=False)
     def website_form(self, model_name, **kwargs):
         print(kwargs)
-        return 200
+        return ksh("kshitij")
