c43c0310ee873c62b6f56cb2789a719623bb40e4
Ranjeet Kumar <ranjeet.kumar@vemo.com>
2021-03-25 01:02:07 +0530

    update

--- custom_seedforce/controllers/test.py
+++ custom_seedforce/controllers/test.py
@@ -16,7 +16,7 @@ class PartnerForm(http.Controller):
     #next controller with url for submitting data from the form#
     def customer_form_submit(self, **post):
         partner = request.env['sale.order'].create({
-            'name': post.get('partner_id'),
+            'name': post.get('partner_id.name'),
             #'email': post.get('email'),
             #'phone': post.get('phone')
         })
