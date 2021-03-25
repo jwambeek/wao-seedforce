acb902f650448bb7df894a988456d93aa90076ee
Kshitij Saini <kshitij.saini.astrea@gmail.com>
2021-03-25 12:31:41 +0530

    Update controllers.py

--- kshitij/controllers/controllers.py
+++ kshitij/controllers/controllers.py
@@ -2,6 +2,7 @@
 # from odoo import http
 from odoo import http
 from odoo.http import request
+import json
 from odoo.addons.website_form.controllers.main import WebsiteForm
 
 def ksh(t):
@@ -11,9 +12,10 @@ class SubmissionTest(WebsiteForm):
     @http.route('/website_form', type='http', auth="public", methods=['POST'], multilang=False)
     def website_form_empty(self, **kwargs):
         print(kwargs)
-        return 200
+        return ""
 
-    @http.route('/website_form/<string:model_name>', type='http', auth="public", methods=['POST'], website=True, csrf=False)
-    def website_form(self, model_name, **kwargs):
-        print(kwargs)
-        return 200
+    # @http.route('/website_form/<string:model_name>', type='http', auth="public", methods=['POST'], website=True, csrf=False)
+    # def website_form(self, model_name, **kwargs):
+    #     print(kwargs)
+    #     # request.env[sale.order].sudo().create()
+    #     return json.dumps({'id': 2000})
