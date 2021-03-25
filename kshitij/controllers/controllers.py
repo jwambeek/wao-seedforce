dbd87f720f46c8b23a9cdb07bbe966d237f23fd2
Kshitij Saini <kshitij.saini.astrea@gmail.com>
2021-03-25 13:27:26 +0530

    Update controllers.py

--- kshitij/controllers/controllers.py
+++ kshitij/controllers/controllers.py
@@ -17,7 +17,13 @@ class SubmissionTest(WebsiteForm):
     @http.route('/website_form/<string:model_name>', type='http', auth="public", methods=['POST'], website=True, csrf=False)
     def website_form(self, model_name, **kwargs):
         data = {
-            "x_studio_opening_balance_so":1000
+            "x_studio_opening_balance_so":1000,
+
         }
         request.env['sale.order'].sudo().create(data)
         return ""
+
+    @http.route(['/shop/cart/update'], type='http', auth="public", methods=['POST'], website=True)
+    def cart_update(self, product_id, add_qty=1, set_qty=0, **kw):
+        _logger.debug(kw)
+        _logger.info(kw)
