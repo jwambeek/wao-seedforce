ae779769739390f85decd72b0c57d01b705cb276
Kshitij Saini <kshitij.saini.astrea@gmail.com>
2021-03-26 13:02:04 +0530

    Update controllers.py

--- kshitij/controllers/controllers.py
+++ kshitij/controllers/controllers.py
@@ -13,12 +13,14 @@ class WebsiteFormTest(WebsiteForm):
 
     @http.route('/website_form/<string:model_name>', type='http', auth="public", methods=['POST'], website=True, csrf=False)
     def website_form(self, model_name, **kwargs):
+        """ this function captures the value of the harvest declaration form. kwargs contains all the values of the form"""
         super_website = super(WebsiteFormTest,self).website_form("res.partner",**kwargs)
         
         _logger.info(kwargs)
         return super_website
 
 class WebsiteSaleTest(WebsiteSale):
+    """ this function get executed whenever we click submit button on the cart  """
     @http.route()
     def checkout(self, **post):
         sup=super(WebsiteSaleTest,self).checkout(**post)
@@ -33,8 +35,10 @@ class WebsiteSaleTest(WebsiteSale):
         res_super=super(WebsiteSaleTest, self).cart()
         return res_super
 
+    # this function works in updating the cart and store the values to the db
     @http.route()
     def cart_update_json(self, product_id, line_id=None, add_qty=None, set_qty=None, display=True):
+        """ whenever we update the product quantity this hook fires and updates the value in the frontEnd as well as in the db """
         updated_cart = super(WebsiteSaleTest,self).cart_update_json(product_id, line_id, add_qty, set_qty)
         _logger.info(product_id)
         _logger.info(line_id)
