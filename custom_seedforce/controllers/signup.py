6db5a376f92a34907ddbd6e84280d9ed93cf6576
Shalini <shalini.astreait@gmail.com>
2021-04-03 01:53:36 +0530

    test003

--- custom_seedforce/controllers/signup.py
+++ custom_seedforce/controllers/signup.py
@@ -2,14 +2,18 @@ from odoo import http, _
 from odoo.http import request
 from odoo.exceptions import UserError
 
+from odoo.addons.auth_signup.models.res_users import SignupError
+from odoo.addons.web.controllers.main import ensure_db, Home
+from odoo.addons.base_setup.controllers.main import BaseSetup
+
 import logging
 
 _logger = logging.getLogger(__name__)
 
-class SignUpForm(http.Controller):
+class SignUpForm(Home):
     def do_signup(self, qcontext):
         """ Shared helper that creates a res.partner out of a token """
-        values = { key: qcontext.get(key) for key in ('login', 'name','password', 'x_studio_ngr_number') }
+        values = { key: qcontext.get(key) for key in ('login', 'name','x_studio_ngr_number', 'password') }
         if not values:
             raise UserError(_("The form was not properly filled in."))
         if values.get('password') != qcontext.get('confirm_password'):
@@ -20,3 +24,11 @@ class SignUpForm(http.Controller):
             values['lang'] = lang
         self._signup_with_values(qcontext.get('token'), values)
         request.env.cr.commit()
+
+    def _signup_with_values(self, token, values):
+        db, login,x_studio_ngr_number, password = request.env['res.users'].sudo().signup(values, token)
+        request.env.cr.commit()     # as authenticate will use its own cursor we need to commit the current transaction
+        uid = request.session.authenticate(db, login, password)
+        if not uid:
+            raise SignupError(_('Authentication Failed.'))
+
