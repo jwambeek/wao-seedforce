40b01f6287b96df83f259a8b1ac923154fe020cc
Shalini <shalini.astreait@gmail.com>
2021-04-03 01:33:54 +0530

    update edits

--- custom_seedforce/__manifest__.py
+++ custom_seedforce/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name' : 'SeedForce',
-    'version': '1.7',
+    'version': '1.8',
     'Summary': '',
     'description': 'Pass the field value into sale order',
     'license': 'LGPL-3',
---
+++ custom_seedforce/controllers/signup.py
@@ -0,0 +1,22 @@
+from odoo import http, _
+from odoo.http import request
+from odoo.exceptions import UserError
+
+import logging
+
+_logger = logging.getLogger(__name__)
+
+class SignUpForm(http.Controller):
+    def do_signup(self, qcontext):
+        """ Shared helper that creates a res.partner out of a token """
+        values = { key: qcontext.get(key) for key in ('login', 'name','x_studio_ngr_number', 'password') }
+        if not values:
+            raise UserError(_("The form was not properly filled in."))
+        if values.get('password') != qcontext.get('confirm_password'):
+            raise UserError(_("Passwords do not match; please retype them."))
+        supported_lang_codes = [code for code, _ in request.env['res.lang'].get_installed()]
+        lang = request.context.get('lang', '')
+        if lang in supported_lang_codes:
+            values['lang'] = lang
+        self._signup_with_values(qcontext.get('token'), values)
+        request.env.cr.commit()
