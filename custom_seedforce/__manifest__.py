61aa8013e32765bfb98bd75a9723e6e0e1c16324
Shalini <shalini.astreait@gmail.com>
2021-04-02 14:48:22 +0530

    update

---
+++ custom_seedforce/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name' : 'SeedForce',
+    'version': '1.7',
+    'Summary': '',
+    'description': 'Pass the field value into sale order',
+    'license': 'LGPL-3',
+    'depends': [
+        'account_accountant',
+        'purchase',
+        'website',
+        'contacts',
+        'sale_management',
+        'product',
+    ],    
+     'data': [
+        #'views/website_form.xml',
+        'views/test_form.xml',
+        'views/sale_harvest_view.xml',
+
+
+    ],
+    'installable': True,
+    'application':True,
+    'auto_install':False
+}
