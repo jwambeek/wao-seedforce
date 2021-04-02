4eaff4c5b5b34d7f3e809974d1139290778cdc13
Shalini <shalini.astreait@gmail.com>
2021-04-03 01:45:29 +0530

    updates

--- custom_seedforce/controllers/signup.py
+++ custom_seedforce/controllers/signup.py
@@ -9,7 +9,7 @@ _logger = logging.getLogger(__name__)
 class SignUpForm(http.Controller):
     def do_signup(self, qcontext):
         """ Shared helper that creates a res.partner out of a token """
-        values = { key: qcontext.get(key) for key in ('login', 'name','x_studio_ngr_number', 'password') }
+        values = { key: qcontext.get(key) for key in ('login', 'name','password', 'x_studio_ngr_number') }
         if not values:
             raise UserError(_("The form was not properly filled in."))
         if values.get('password') != qcontext.get('confirm_password'):
