ef749f55d6aa45cb36b6ded8d80a451b1519275c
akshi2501 <74041011+akshi2501@users.noreply.github.com>
2021-04-02 13:15:21 +0530

    update

---
+++ custom_seedforce/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name' : 'SeedForce',
+    'version': '1.7',
+    'Summary': '',
+    'description': 'Pass the field value into sale order',
+    'license': 'LGPL-3',
+    'depends': [
+        'account_accountant',
+        'purchase',
+        'website',
+        'contacts',
+        'sale_management',
+        'product',
+    ],    
+     'data': [
+
+        'views/signup.xml',
+
+
+    ],
+    'installable': True,
+    'application':True,
+    'auto_install':False
+}
