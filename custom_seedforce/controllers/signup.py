d8ab58a2211d87e155659abf8bb0efd543f746a5
Shalini <shalini.astreait@gmail.com>
2021-04-02 13:59:56 +0530

    created controller

---
+++ custom_seedforce/controllers/signup.py
@@ -0,0 +1,28 @@
+import logging
+import werkzeug
+
+from odoo import http, _
+from odoo.addons.auth_signup.models.res_users import SignupError
+from odoo.addons.web.controllers.main import ensure_db, Home
+from odoo.addons.base_setup.controllers.main import BaseSetup
+from odoo.exceptions import UserError
+from odoo.http import request
+
+_logger = logging.getLogger(__name__)
+
+
+class AuthSignup(Home):
+    def do_signup(self, qcontext):
+        """ Shared helper that creates a res.partner out of a token """
+        values = { key: qcontext.get(key) for key in ('login', 'name','address', 'password') }
+        if not values:
+            raise UserError(_("The form was not properly filled in."))
+        if values.get('password') != qcontext.get('confirm_password'):
+            raise UserError(_("Passwords do not match; please retype them."))
+        supported_lang_codes = [code for code, _ in request.env['res.lang'].get_installed()]
+        lang = request.context.get('lang', '')
+        if lang in supported_lang_codes:
+            values['lang'] = lang
+        self._signup_with_values(qcontext.get('token'), values)
+        request.env.cr.commit()
+
