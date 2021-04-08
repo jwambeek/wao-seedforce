796b051a85489a1ac405c9ea5cf425eca2dc8a5e
jwambeek <70319876+jwambeek@users.noreply.github.com>
2021-04-08 17:17:16 +0530

    test

--- custom_seedforce/controllers/testController.py
+++ custom_seedforce/controllers/testController.py
@@ -2,7 +2,7 @@ from odoo import http
 from odoo.http import request
 
 class GetData(http.Controller):
-     @http.route('/shop/address', type='http', website=True, auth='public')
+     @http.route('/shop/payment', type='http', website=True, auth='public')
      def index(self, **kw):
          return "Hello, world"
        
