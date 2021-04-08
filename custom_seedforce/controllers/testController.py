ade9cb325bf1fccd95ecdad1e4b5bb00adcd8fa1
jwambeek <70319876+jwambeek@users.noreply.github.com>
2021-04-08 18:18:04 +0530

    test_1

--- custom_seedforce/controllers/testController.py
+++ custom_seedforce/controllers/testController.py
@@ -4,7 +4,8 @@ from odoo.http import request
 class GetData(http.Controller):
      @http.route('/shop/payment', type='http', website=True, auth='public')
      def index(self, **kw):
-         return "Hello, world"
+         #return "Hello, world"
+         return request.render("custom_seedforce.get_data_form" , {})
        
 
 
