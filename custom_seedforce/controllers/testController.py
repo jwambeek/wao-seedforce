314b9cd710997af126deadb4bb23cc2357d7939e
jwambeek <70319876+jwambeek@users.noreply.github.com>
2021-04-08 18:20:01 +0530

    test_2

--- custom_seedforce/controllers/testController.py
+++ custom_seedforce/controllers/testController.py
@@ -5,12 +5,4 @@ class GetData(http.Controller):
      @http.route('/shop/payment', type='http', website=True, auth='public')
      def index(self, **kw):
          #return "Hello, world"
-         return request.render("custom_seedforce.get_data_form" , {})
-       
-
-
-
-
-		
-
-
+        return request.render("custom_seedforce.get_data_form" , {})
