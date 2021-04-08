c4b70d03ddf5b5200249f8381c96b9f8cc7ae3a4
jwambeek <70319876+jwambeek@users.noreply.github.com>
2021-04-08 15:58:25 +0530

    test

--- custom_seedforce/controllers/main_harvest.py
+++ custom_seedforce/controllers/main_harvest.py
@@ -1,8 +1,8 @@
 from odoo import http
 from odoo.http import request
 
-class WebsiteSale(http.Controller):
-    @http.route('/shop/payment',website=True, auth='public')
+#class WebsiteSale(http.Controller):
+    @http.route(['/shop/payment'],website=True, auth='public')
     def get_data(self, **kw):
          return "Hello, world"
         #return request.render("recently_purchased.test_page" , {})
