acb5e3ac2b24c4e125771d68d9ed518f87fdd06b
Akash Dixit <akash.astreait@gmail.com>
2021-04-09 17:03:33 +0530

    test

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name' : 'SeedForce',
-    'version': '3.13',
+    'version': '3.14',
     'Summary': '',
     'description': 'Pass the field value into sale order',
     'license': 'LGPL-3',
