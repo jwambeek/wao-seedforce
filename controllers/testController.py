d3ec4ee4ad23b19b7ca2c1b582c5390ee9cb63a4
Akash Dixit <akash.astreait@gmail.com>
2021-04-09 11:01:08 +0530

    test

--- controllers/testController.py
+++ controllers/testController.py
@@ -1,8 +1,8 @@
-from odoo import http
-from odoo.http import request
+#from odoo import http
+#from odoo.http import request
 
-class GetData(http.Controller):
-     @http.route('/shop/payment', type='http', website=True, auth='public')
-     def index(self, **kw):
+#class GetData(http.Controller):
+ #    @http.route('/shop/payment', type='http', website=True, auth='public')
+  #   def index(self, **kw):
          #return "Hello, world"
-        return request.render("custom_seedforce.get_data_form" , {})
+   #     return request.render("custom_seedforce.get_data_form" , {})
