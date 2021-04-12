3bd53534b572292ae9b8b7e5486e1934adf32478
Akash Dixit <akash.astreait@gmail.com>
2021-04-12 13:06:01 +0530

    test10

--- custom_seedforce/__manifest__.py
+++ custom_seedforce/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name' : 'SeedForce',
-    'version': '3.4',
+    'version': '3.5',
     'Summary': '',
     'description': 'Pass the field value into sale order',
     'license': 'LGPL-3',
@@ -17,6 +17,7 @@
         #'views/test_form.xml',
         #'views/sale_harvest_view.xml',
         'views/template.xml',
+        'views/contact_form.xml',
         'views/signup_template.xml'
         
          ],
