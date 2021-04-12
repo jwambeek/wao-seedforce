546254dedf364680b6c8981d57b2d2c18bfcca34
Shalini <shalini.astreait@gmail.com>
2021-04-12 16:29:18 +0530

    update

--- custom_seedforce/__manifest__.py
+++ custom_seedforce/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name' : 'SeedForce',
-    'version': '5.5',
+    'version': '5.6',
     'Summary': '',
     'description': 'Pass the field value into sale order',
     'license': 'LGPL-3',
--- custom_seedforce/static/src/js/cart_table.js
+++ custom_seedforce/static/src/js/cart_table.js
@@ -14,7 +14,7 @@ $(document).ready(()=>{
         $('#qty_delivered').val('')
         $('#other').val('')
         $('#qty_retained').val('')
-    console.log(ks)
+    //console.log(ks)
 
     })
     $('#form_submit_btn').click((e)=>{
