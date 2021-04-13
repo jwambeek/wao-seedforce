7d63599cf64ea15a90bdf8c947ab1b52ae28e2a7
Shalini <shalini.astreait@gmail.com>
2021-04-13 12:03:19 +0530

    delete

--- custom_seedforce/controllers/testController.py
+++
@@ -1,8 +0,0 @@
-from odoo import http
-from odoo.http import request
-
-class GetData(http.Controller):
-     @http.route('/shop/payment', type='http', website=True, auth='public')
-     def index(self, **kw):
-         #return "Hello, world"
-        return request.render("custom_seedforce.get_data_form" , {})
