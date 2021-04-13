f8b6a5164a5031a5e5ede01ef92aca2f8920b372
jwambeek <70319876+jwambeek@users.noreply.github.com>
2021-04-13 17:39:29 +0530

    test19

--- custom_seedforce/controllers/testController.py
+++ custom_seedforce/controllers/testController.py
@@ -1,8 +1,19 @@
 from odoo import http
 from odoo.http import request
+from odoo.addons.website_sale.controllers.main import WebsiteSale 
 
-class GetData(http.Controller):
-     @http.route('/shop/payment', type='http', website=True, auth='public')
-     def index(self, **kw):
+
+class WebsiteSelData(WebsiteSale):
+	 @http.route(['/shop/payment'], type='http', auth="public", website=True, sitemap=False)
+      def payment(self, **post):
+     	res = super(WebsiteSelData,self).payment(self, **post)
+     	print("odoo", res)
+     	return res
+     #	return "Odoo"
+     	#return request.render("custom_seedforce.get_data_form" , {})
+
+#class GetData(http.Controller):
+ #    @http.route('/shop/payment', type='http', website=True, auth='public')
+  #   def index(self, **kw):
          #return "Hello, world"
-        return request.render("custom_seedforce.get_data_form" , {})
+   #     return request.render("custom_seedforce.get_data_form" , {})
