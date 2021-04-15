14d3021631d84becf810fa78191876cc99cdd819
jwambeek <70319876+jwambeek@users.noreply.github.com>
2021-04-15 11:34:50 +0530

    test26

--- custom_seedforce/__manifest__.py
+++ custom_seedforce/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name' : 'SeedForce',
-    'version': '4.10',
+    'version': '4.11',
     'Summary': '',
     'description': 'Pass the field value into sale order',
     'license': 'LGPL-3',
